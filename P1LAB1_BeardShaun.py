205552b44488680d70abca984bf2ccf42bbd03a6
Shaunyboi353 <beards3648@student.faytechcc.edu>
2025-02-11 10:59:20 -0500

    Add files via upload

---
+++ P1LAB1_BeardShaun.py
@@ -0,0 +1,11 @@
+ # Shaun Beard
+ # 2/4/2025
+ # P1LAB1
+ # using input and print function to display names
+
+# inputs names
+user_first_name = input("Enter your first name: ")
+user_Last_name = input("Enter your last name: ")
+
+# The welcome print
+print("hello,", user_first_name, user_Last_name + "!", "Welcome to CTI-110")
