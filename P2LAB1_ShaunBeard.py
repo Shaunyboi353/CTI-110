39743d868d2937893fc20bd500b170e375834964
Shaunyboi353 <beards3648@student.faytechcc.edu>
2025-02-25 11:55:52 -0500

    Add files via upload

---
+++ P2LAB1_ShaunBeard.py
@@ -0,0 +1,8 @@
+# Shaun Beard
+# 2/25/2025
+# P2LAB2
+# uses a dictionary to store user input and displays output to the user
+
+dict = {"Camaro": 18.21, "Prius": 52.36, "Model S": 110, "Silverado": 26 }
+print(dict.keys())
+cars = input("Enter a vehicle to see it's mpg: ")
